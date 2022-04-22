4829ebbcb821a9564d99482ed10efee7ef4a94c7
Yesterday17 <mmf@mmf.moe>
2022-04-23 01:19:01 +0800

    AES 加密解密

--- packages/node/src/services/encrypt.ts
+++ packages/node/src/services/encrypt.ts
@@ -1,18 +1,29 @@
 import * as crypto from "crypto";
+import { md5 } from "./hash";
 
-function encrypt(key: string, data: string) {
-  const iv = crypto.randomBytes(12);
-  const cipher = crypto.createCipheriv("aes-256-gcm", key, iv);
+export function encrypt(key: string, data: Record<string, any>) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv("aes-256-gcm", md5(key), iv);
 
-  let enc = cipher.update(data, "utf8", "binary");
-  enc += cipher.final("binary");
-  return [enc, iv, cipher.getAuthTag()];
+  const text = JSON.stringify(data);
+  const encrypted = Buffer.concat([
+    iv,
+    cipher.getAuthTag(),
+    cipher.update(text),
+    cipher.final(),
+  ]);
+  return encrypted;
 }
 
-// function decrypt(key: string, data: string) {
-//   const decipher = crypto.createDecipheriv(ALGO, key, iv);
-//   decipher.setAuthTag(authTag);
-//   let str = decipher.update(enc, "base64", "utf8");
-//   str += decipher.final("utf8");
-//   return str;
-// }
+export function decryptText<T>(key: string, data: Buffer): T {
+  const iv = data.slice(0, 16);
+  const tag = data.slice(16, 32);
+  const encrypted = data.slice(32);
+  const decipher = crypto.createDecipheriv("aes-256-gcm", md5(key), iv);
+  decipher.setAuthTag(tag);
+  const decrypted = Buffer.concat([
+    decipher.update(encrypted),
+    decipher.final(),
+  ]);
+  return JSON.parse(decrypted.toString("utf-8"));
+}
