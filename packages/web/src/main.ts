a15ca9e94cdf854b197abab1472ae6fa1e7b8a6f
Yesterday17 <mmf@mmf.moe>
2022-04-25 01:11:49 +0800

    密码输入

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,10 +1,18 @@
+// MDUI 样式部分
+// @ts-ignore
 import mdui from "mdui";
+import "mdui/dist/css/mdui.css";
 
+// 自定义样式
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+document
+  .querySelector("#gr-submit-password")!
+  .addEventListener("click", submitPassword);
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+function submitPassword() {
+  const passwordInput =
+    document.querySelector<HTMLInputElement>("#gr-password");
+  const password = passwordInput!.value;
+  console.log(password);
+}
