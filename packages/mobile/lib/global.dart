e8b4a44e82d0977c74136765ba4142e42e5f2459
Yesterday17 <mmf@mmf.moe>
2022-04-27 21:24:21 +0800

    客户端 目录视图

---
+++ packages/mobile/lib/global.dart
@@ -0,0 +1,6 @@
+import 'package:webdav_client/webdav_client.dart';
+
+class Global {
+  static Client client =
+      newClient("http://192.168.1.9:3010/MgKq_VIb4BLr5hnxdkjzDw/drive/1/");
+}
---
+++ packages/mobile/lib/pages/folder.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile/global.dart';
+import 'package:webdav_client/webdav_client.dart';
+import 'package:path/path.dart' as p;
+
+class FolderView extends StatefulWidget {
+  const FolderView({Key? key}) : super(key: key);
+
+  @override
+  _FolderViewState createState() => _FolderViewState();
+}
+
+class _FolderViewState extends State<FolderView> {
+  List<File> content = [];
+  String path = "/";
+  bool loading = true;
+  bool showHiddenFiles = false;
+
+  @override
+  void initState() {
+    super.initState();
+    readDir();
+  }
+
+  void readDir() async {
+    try {
+      final List<File> files = await Global.client.readDir(path);
+      files.removeWhere((element) =>
+          !showHiddenFiles ? element.name!.startsWith(".") : false);
+      print(path);
+      if (path != "/") {
+        files.insert(0, File(name: "..", isDir: true));
+      }
+      setState(() {
+        content.replaceRange(0, content.length, files);
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final item = content[index];
+        return ListTile(
+          leading: item.isDir ?? false
+              ? const Icon(Icons.folder)
+              : const Icon(Icons.insert_drive_file),
+          title: Text(item.name!),
+          onTap: () {
+            if (item.isDir!) {
+              path = p.normalize(p.join(path, item.name!));
+              readDir();
+            } else {
+              // TODO: open file
+            }
+          },
+        );
+      },
+      itemCount: content.length,
+    );
+  }
+}
