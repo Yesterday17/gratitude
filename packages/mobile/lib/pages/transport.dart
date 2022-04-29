23f67ce99545e6eafaa8e521bb85e63086374f95
Yesterday17 <mmf@mmf.moe>
2022-04-29 15:34:54 +0800

    传输页框架

--- packages/mobile/lib/pages/transport.dart
+++ packages/mobile/lib/pages/transport.dart
@@ -10,9 +10,25 @@ class TransportPage extends StatefulWidget {
 class _TransportPageState extends State<TransportPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("文件传输")),
-      // body: ,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("文件传输"),
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Text("下载")),
+              Tab(child: Text("上传")),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Icon(Icons.download),
+            Icon(Icons.upload),
+          ],
+        ),
+      ),
     );
   }
 }
