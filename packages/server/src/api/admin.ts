53541c92f8f582c4f38f03f6140d07620bda34e6
Yesterday17 <mmf@mmf.moe>
2022-06-08 12:09:04 +0800

    fix

--- packages/server/src/api/admin.ts
+++ packages/server/src/api/admin.ts
@@ -10,7 +10,6 @@ router.post("/drive", async (req, res) => {
   const data: DriveCreateRequest = req.body;
 
   await database.createDrive(data.name, data.path);
-  // TODO: create webdav
   res.json({ code: 0, data: {} });
 });
 
@@ -18,7 +17,6 @@ router.delete("/drive/:id", async (req, res) => {
   const database = await db;
   const id = parseInt(req.params.id);
   await database.removeDrive(id);
-  // TODO: remove webdav
   res.json({ code: 0, data: {} });
 });
 
